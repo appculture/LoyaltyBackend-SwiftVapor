e43ca9635a6e31b2ecfc6b636c1b31547f207404
Marko Tadic <marko.tadic@appculture.com>
2016-08-16 14:19:33 +0200

    Initialized new project with Vapor

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "Loyalty",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16),
+        .Package(url: "https://github.com/vapor/vapor-mustache.git", majorVersion: 0, minor: 11)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
+
