412d293d2390e977ffd9da3497dd10d6d2c352e6
Marko Tadic <marko.tadic@appculture.com>
2016-08-19 11:46:05 +0200

    Renamed app from Loyalty to Royalty Removed Vapor boilerplate Added initial version of API documentation (Apiary)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Loyalty",
+    name: "Royalty",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16),
         .Package(url: "https://github.com/vapor/vapor-mustache.git", majorVersion: 0, minor: 11),
