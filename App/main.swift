976c1b29ca1dffa1812b3d1cfe604369c4809c3a
Marko Tadić <tadija@me.com>
2016-08-21 21:27:40 +0200

    Major cleanup: removed all stuff related to MongoDB

--- App/main.swift
+++ App/main.swift
@@ -1,8 +1,8 @@
 import Vapor
-import VaporMustache
-import HTTP
 import Fluent
 import VaporMySQL
+import VaporMustache
+import HTTP
 
 let preparations: [Preparation.Type] = [Customer.self]
 let providers: [Vapor.Provider.Type] = [VaporMustache.Provider.self, VaporMySQL.Provider.self]
@@ -11,44 +11,6 @@ let drop = Droplet(preparations: preparations, providers: providers)
 drop.get("/") { request in
     return "Hello, Royalty!"
 }
-
-/*
-drop.get("mongo") { request in
-    let customerDocuments = try MongoDB.shared.Customer.find()
-    let json = customerDocuments.makeDocument().makeExtendedJSON()
-    return json
-}
-
-drop.get("mongo-customers") { request in
-    let customerDocuments = try MongoDB.shared.Customer.find()
-    let customerList = Array(customerDocuments).map { Customer.init(document: $0) }
-    let customerListJSON = customerList.flatMap { try? $0!.makeJSON() }
-    return JSON(["Customers" : .array(customerListJSON)])
-}
-
-let customerCollection = CustomerCollection()
-drop.collection(customerCollection)
-*/
- 
-/*
-drop.post("customers") { request in
-    guard
-        let first = request.data["first"].string,
-        let last = request.data["last"].string,
-        let email = request.data["email"].string,
-        let password = request.data["password"].string
-    else {
-        throw Abort.badRequest
-    }
-    
-    return try JSON([
-        "first": first,
-        "last": last,
-        "email": email,
-        "password": password,
-    ])
-}
-*/
 
 let customers = CustomerController(droplet: drop)
 drop.resource("customers", customers)
