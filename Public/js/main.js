5be62c05a0450bf93f0569e1a3db788680620782
Marko Tadic <marko.tadic@appculture.com>
2016-08-22 18:00:49 +0200

    Included jQuery and main.js to footer.mustache Implemented link to customer details on row click in customers table Renamed public directories: images -> img, styles -> css, added js

---
+++ Public/js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+                       
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+                       
+});
