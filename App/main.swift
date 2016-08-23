050448d1e80076e5dc069e009d3859b502fb3bf8
Marko Tadić <tadija@me.com>
2016-08-24 00:10:14 +0200

    Created Router class and refactored handling routes from main.swift there

--- App/main.swift
+++ App/main.swift
@@ -4,73 +4,24 @@ import VaporMySQL
 import VaporMustache
 import HTTP
 
-// MARK: - Configuration
+// MARK: - Create Droplet
 
 let mustache = VaporMustache.Provider(withIncludes: [
     "header" : "Includes/header.mustache",
     "footer" : "Includes/footer.mustache"
 ])
 
-let preparations: [Preparation.Type] = [Customer.self, Purchase.self, Voucher.self, CustomerSession.self, VoucherConfig.self]
+let preparations: [Preparation.Type] = [Customer.self, Purchase.self, Voucher.self, VoucherConfig.self, CustomerSession.self]
 let providers: [Vapor.Provider.Type] = [VaporMySQL.Provider.self]
 
 let drop = Droplet(preparations: preparations, providers: providers, initializedProviders: [mustache])
 
-// MARK: - Root
+// MARK: - Configure Routes
 
-drop.get("/") { request in
-    return "Hello, Royalty!"
-}
+let router = Router(droplet: drop)
+router.configureRoutes()
 
-// MARK: - Customers
-
-let customers = CustomerController(droplet: drop)
-drop.resource("customers", customers)
-
-drop.post("customers/login") { request in
-    return try customers.login(request: request)
-}
-
-drop.post("customers/logout") { request in
-    return try customers.logout(request: request)
-}
-
-drop.post("customers", Customer.self, "purchases") { request, customer in
-    return try customer.purchases().all().makeResponse()
-}
-
-drop.post("customers", Customer.self, "vouchers") { request, customer in
-    return try customer.vouchers().all().makeResponse()
-}
-
-let customerMiddleware = CustomerMiddleware(droplet: drop)
-drop.middleware.append(customerMiddleware)
-
-// MARK: - Purchases
-
-let purchases = PurchaseController(droplet: drop)
-drop.resource("purchases", purchases)
-
-let purchaseMiddleware = PurchaseMiddleware(droplet: drop)
-drop.middleware.append(purchaseMiddleware)
-
-// MARK: - Vouchers
-
-let vouchers = VoucherController(droplet: drop)
-drop.resource("vouchers", vouchers)
-
-drop.get("vouchers/config") { request in
-    return try vouchers.getConfig(request: request)
-}
-
-drop.put("vouchers/config") { request in
-    return try vouchers.editConfig(request: request)
-}
-
-let voucherMiddleware = VoucherMiddleware(droplet: drop)
-drop.middleware.append(voucherMiddleware)
-
-// MARK: - Serve
+// MARK: - Serve Droplet
 
 let port = drop.config["app", "port"].int ?? 80
 
