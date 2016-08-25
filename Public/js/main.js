f649e7b4001d864622221b83c9e0be77e0d2dc41
Marko Tadic <marko.tadic@appculture.com>
2016-08-25 15:30:47 +0200

    Implemented client side validation to allow only numbers in Edit Config form

--- Public/js/main.js
+++ Public/js/main.js
@@ -7,5 +7,26 @@ jQuery(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
     });
+
+    // client side validation to allow only numbers in textfield
+    $(".numberField").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl+A, Command+A
+            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+            // Allow: Ctrl+C, Command+C
+            (e.keyCode === 67 && (e.ctrlKey === true || e.metaKey === true)) || 
+            // Allow: Ctrl+X, Command+X
+            (e.keyCode === 88 && (e.ctrlKey === true || e.metaKey === true)) || 
+             // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                // let it happen, don't do anything
+                return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
                        
 });
